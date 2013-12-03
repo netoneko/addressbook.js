a1d53484189e0da7be3ec19ee8ab656f1c960254
Kirill Maksimov <k.maksimov@applicaster.com>
2013-12-03 19:56:24 +0200

    Add prompt

--- addressbook.js
+++ addressbook.js
@@ -2,3 +2,21 @@ exports.hello = function() {
   return "Hello, World!";
 }
 
+var cli = function() {
+  var readline = require('readline').createInterface(process.stdin, process.stdout);
+
+  readline.setPrompt('> ');
+  readline.prompt();
+
+  readline.on('line', function(input) {
+    var line = input.trim();
+    console.log(line);
+    readline.prompt();
+  }).on('close', function() {
+    console.log('Bye!');
+    process.exit(0);
+  });
+};
+
+exports.cli = cli;
+
---
+++ cli.js
@@ -0,0 +1,3 @@
+var ab = require("./addressbook");
+ab.cli();
+
